c50850e5bd461a761c3cd2810bc9535b7aafcfad
saimon2588 <77831634+saimon2588@users.noreply.github.com>
2021-11-07 02:07:51 +0300

    add command

--- az104-08-install_IIS.ps1
+++ az104-08-install_IIS.ps1
@@ -1,1 +1,4 @@
 
+powershell.exe Install-WindowsFeature -name Web-Server -IncludeManagementTools
+powershell.exe Remove-Item -Path 'C:\inetpub\wwwroot\iisstart.htm'
+powershell.exe Add-Content -Path 'C:\inetpub\wwwroot\iisstart.htm' -Value "$env:computername"
